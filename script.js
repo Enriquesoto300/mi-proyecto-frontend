b5e47f04a3c928695ebb7cb8081c20614046deea
Enriqueso300 <24040001@utculiacan.edu.mx>
2025-11-15 15:17:25 -0700

    Primer commit frontend

---
+++ script.js
@@ -0,0 +1,29 @@
+// NOTA IMPORTANTE:
+// Cuando subas esto a la nube, cambiarás esta URL por la de Render.
+// Por ahora, usamos la local.
+const API_URL = "http://127.0.0.1:5000/api/saludo";
+
+async function obtenerDatos() {
+    const caja = document.getElementById("resultado");
+    caja.innerText = "Cargando...";
+
+    try {
+        // 1. Hacemos la petición al servidor
+        const respuesta = await fetch(API_URL);
+        
+        // 2. Verificamos si respondió bien
+        if (!respuesta.ok) {
+            throw new Error("Error en la red");
+        }
+
+        // 3. Convertimos la respuesta a JSON (texto legible)
+        const datos = await respuesta.json();
+
+        // 4. Mostramos los datos en el HTML
+        caja.innerText = `Mensaje: ${datos.mensaje} \n Estado: ${datos.estado}`;
+        
+    } catch (error) {
+        caja.innerText = "Error: No se pudo conectar con el backend. ¿Está encendido?";
+        console.error("Hubo un error:", error);
+    }
+}
